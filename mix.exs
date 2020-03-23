b0beea8b4acfe8ab0897edea6363268c1db5c948
hobertro <boho527@gmail.com>
2020-03-22 19:59:08 -0700

    added floki

--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule Covid626.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:crawly, "~> 0.1"}
+      {:crawly, "~> 0.1"},
+      {:floki, "~> 0.26.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
