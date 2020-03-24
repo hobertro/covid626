c9550dd53cb5a34db464eaec522501acd5fbfe56
hobertro <boho527@gmail.com>
2020-03-23 23:50:10 -0700

    experiment with using Enum any, member, Tuple to list, filter

--- lib/covid626.ex
+++ lib/covid626.ex
@@ -1,18 +1,26 @@
 defmodule Covid626 do
-  @moduledoc """
-  Documentation for `Covid626`.
-  """
+  
+  @san_gabriel_valley ["Alhambra", "Altadena", "Pasadena", "Monterey Park", "Arcadia", "Rosemead", 
+  "El Monte", "South El Monte", "South Pasadena", "Temple City", "West Covina",
+  "Rowland Heights", "Monrovia", "Walnut"]
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Covid626.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def get_cases_url() do
+    case HTTPoison.get("http://www.publichealth.lacounty.gov/media/Coronavirus/locations.htm") do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        response = body |> Floki.find("tr")
+        response = Enum.filter(response, fn row ->
+          list1 = Tuple.to_list(row)
+          Enum.any?(list1, fn thing ->
+IO.inspect thing
+            testz = Enum.member?(@san_gabriel_valley, thing)
+            Enum.member?(@san_gabriel_valley, thing)
+          end)
+        end)
+        {:ok, response}
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        IO.puts "Not found"
+      {:error, %HTTPoison.Error{ reason: reason}} ->
+        IO.inspect reason
+    end
   end
 end
