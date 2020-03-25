6ef80db5e1cf8428bd5cb69eaea214e108ac964c
hobertro <boho527@gmail.com>
2020-03-24 22:44:17 -0700

    crawl site to get 626 related cases and info

--- lib/covid626.ex
+++ lib/covid626.ex
@@ -1,26 +1,55 @@
 defmodule Covid626 do
-  
-  @san_gabriel_valley ["Alhambra", "Altadena", "Pasadena", "Monterey Park", "Arcadia", "Rosemead", 
-  "El Monte", "South El Monte", "South Pasadena", "Temple City", "West Covina",
-  "Rowland Heights", "Monrovia", "Walnut"]
+  @san_gabriel_valley ["Alhambra", "Altadena", "Arcadia", "Avocado Heights", "Azusa", "Baldwin Park", 
+  "Basset", "Bradbury", "Charter Oak", "Citrus", "Covina", "Duarte", "East Pasadena", "East San Gabriel",
+  "El Monte", "Glendora", "Hacienda Heights", "City of Industry", "Industry", "Irwindale", "La Puente",
+  "Mayflower Village", "Monrovia", "Monterey Park", "North El Monte", "El Monte", "Pasadena", "Rosemead",
+  "Rowland Heights", "San Gabriel", "San Marino", "Sierra Madre", "South El Monte", "South Pasadena", "South San Gabriel",
+  "South San Jose Hills", "Temple City", "Valinda", "Vincent", "Walnut", "West Covina", "West Puente Valley"]
 
-  def get_cases_url() do
+  def testz() do
     case HTTPoison.get("http://www.publichealth.lacounty.gov/media/Coronavirus/locations.htm") do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         response = body |> Floki.find("tr")
-        response = Enum.filter(response, fn row ->
-          list1 = Tuple.to_list(row)
-          Enum.any?(list1, fn thing ->
-IO.inspect thing
-            testz = Enum.member?(@san_gabriel_valley, thing)
-            Enum.member?(@san_gabriel_valley, thing)
-          end)
+        response = Enum.map(response, fn row ->
+          {_, _, a} = row # pattern matching to only get the lists of relevant data
+        a
         end)
-        {:ok, response}
+        |> filter_for_sgv
+        |> get_values_for_cities
+        total = total_cases(response)
+        {:ok, response, total}
       {:ok, %HTTPoison.Response{status_code: 404}} ->
         IO.puts "Not found"
       {:error, %HTTPoison.Error{ reason: reason}} ->
         IO.inspect reason
     end
+  end
+
+  def filter_for_sgv(list) do
+    Enum.filter(list, fn value -> 
+      [head | tail] = value
+      {_, _, list} = head
+      [head | tail] = list
+      str = head
+      Enum.member?(@san_gabriel_valley, str)
+    end)
+  end
+
+  def get_values_for_cities(list) do
+    Enum.map(list, fn value -> 
+      [head | tail]  = value
+      {_, _, city}   = head
+      {_, _, number} = List.first(tail)
+      city = List.first(city)
+      number = List.first(number)
+      {city, number}
+    end)
+  end
+
+  def total_cases(list) do
+    Enum.reduce(list, 0, fn(tup, acc) ->
+      {_, value} = tup
+      acc + String.to_integer(value)
+    end)
   end
 end
